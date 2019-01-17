6d23f853c0120f24e2ab0ffbd898a07ee713b452
maincoke <maincoke@gmail.com>
2019-01-16 20:23:45 -0500

    Depuración y proligidad de frontend en index, inclusion de Foundation-Icons

--- client/js/index.js
+++ client/js/index.js
@@ -3,8 +3,8 @@ class Login {
     this.submitEvent();
   }
 
-  submitEvent(){
-    $('form').submit(event => {
+  submitEvent() {
+    $('form#login').submit(event => {
       event.preventDefault();
       this.sendForm();
     });
@@ -36,7 +36,69 @@ class Login {
   }
 }
 
+class Singup {
+  constructor() {
+    this.singInEvent();
+  }
+
+  singInEvent() {
+    $('form#signup').submit(event => {
+      event.preventDefault();
+      let sw1 = $('#passwdusr').val() !== $('#confirmpw').val() ? false : true;
+      let sw2 = $('#condterms').prop('checked');
+      if (sw1 && sw2) {
+        this.userSignUp();
+      } else if (!sw1) {
+        alert('Las contraseñas ingresadas no coinciden!!')
+      } else if (!sw2) {
+        alert('No ha aceptado los términos y codiciones del uso de la Agenda!!');
+      }
+    });
+  }
+
+  userSignUp() {
+    let user_data =new FormData();
+    user_data.append('userfnames', $('#fnamesusr').val() + ' ' + $('#lnamesusr').val());
+    user_data.append('userdbirth', $('#dbirthusr').val());
+    user_data.append('useremaila', $('#emailausr').val());
+    user_data.append('userpasswd', $('#passwdusr').val());
+    $.ajax({
+      url: '../server/create_user.php',
+      type: 'POST',
+      dataType: 'json',
+      cache: false,
+      processData: false,
+      contentType: false,
+      data: user_data,
+      success: function(response) {
+        if (response.msg = 'ok') {
+          alert('Usuario registrado con éxito!!');
+          $('form#signup').reset();
+          $('myModal').foundation('close');
+        } else {
+          alert(response.msg);
+        }
+      },
+      error: function(error) {
+        console.log(error);
+        alert('Hubo un error en la comunicación con el servidor de registro!!');
+      }
+    });
+  }
+}
+
 $(function() {
   $(document).foundation();
+  $('#dbirthusr').fdatepicker({
+    format: 'dd-mm-yyyy',
+    disableDblClickSelection: true,
+    startView: 4,
+    closeButton: true,
+    closeIcon: '<i class = "fi-x"> </i>',
+    leftArrow: '<i class = "fi-rewind"></i>',
+    rightArrow: '<i class = "fi-fast-forward"></i>',
+    language: 'es'
+  });
   var l = new Login();
+  var r = new Singup();
 });
