9878998a7def8a371febaf397512b6f50d03ecb9
maincoke <maincoke@gmail.com>
2019-02-02 22:21:02 -0500

    Actualización de BD, comprobación de funcionalidad e inclusión de archivo txt de GitHub

--- schedule_db.sql
+++ schedule_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 25-01-2019 a las 16:57:44
+-- Tiempo de generación: 03-02-2019 a las 04:02:24
 -- Versión del servidor: 10.1.37-MariaDB
 -- Versión de PHP: 7.1.24
 
@@ -44,36 +44,36 @@ CREATE TABLE `events` (
 --
 
 INSERT INTO `events` (`identevt`, `evttitles`, `evtbgindt`, `evtbgintm`, `evtenddat`, `evtendtim`, `evtfulday`, `fk_iduser`) VALUES
-(1, 'Ensayo para el casting de pelicula', '2019-01-26', '10:00:00', '2019-01-26', '16:00:00', 0, 4),
-(2, 'Asistir a consulta de odontología ', '2019-01-28', '10:00:00', '2019-01-28', '12:00:00', 0, 4),
-(3, 'Reunión con amigos de club en casa de Robert', '2019-01-27', NULL, NULL, NULL, 1, 4),
-(4, 'Asistir a casting al teatro British Hall', '2019-01-28', '14:00:00', '2019-01-28', '18:00:00', 0, 4),
-(5, 'Cumpleaños de Aldus y Brian', '2019-01-29', NULL, NULL, NULL, 1, 4),
-(6, 'Cita con el doctor Dimitri', '2019-01-31', '09:00:00', '2019-01-31', '10:00:00', 0, 4),
-(7, 'Asistir a la Gala de Premios', '2019-02-03', NULL, NULL, NULL, 1, 4),
-(8, 'Preparación para la Gala de premios', '2019-02-02', '17:00:00', '2019-02-02', '20:00:00', 0, 4),
-(9, 'Reunión con la familia', '2019-02-02', NULL, NULL, NULL, 1, 4),
-(10, 'Prueba y ensayo de escena con Vehículo', '2019-02-01', NULL, NULL, NULL, 1, 4),
-(11, 'Comprar regalo para Dorothy', '2019-01-26', '09:00:00', '2019-01-26', '14:00:00', 0, 3),
-(12, 'Cumpleaños de Dorothy', '2019-01-27', NULL, NULL, NULL, 1, 3),
-(13, 'Reunión con mi hija Alice', '2019-01-26', '17:00:00', '2019-01-26', '22:00:00', 0, 3),
-(14, 'Asistir a la consulta dermatológica', '2019-01-28', '08:00:00', '2019-01-28', '10:00:00', 0, 3),
-(15, 'Cumplir con la clase de Actuación y Doblaje', '2019-01-29', NULL, NULL, NULL, 1, 3),
-(16, 'Compras y pagos mensuales', '2019-01-30', NULL, NULL, NULL, 1, 3),
-(17, 'Comienzo de rodaje de la Película', '2019-01-31', '08:00:00', '2019-02-01', '18:00:00', 0, 3),
-(18, 'Reunión con productores y equipo de riesgo', '2019-02-02', '09:00:00', '2019-02-02', '15:00:00', 0, 3),
-(19, 'Rodaje #2 y 3 de película', '2019-02-03', '13:00:00', '2019-02-03', '23:00:00', 0, 3),
-(20, 'Reunión con Patric sobre escena de drama y riesgo', '2019-02-05', NULL, NULL, NULL, 1, 3),
-(21, 'Asistir al evento Comic-Con de Texas', '2019-01-26', NULL, NULL, NULL, 1, 2),
-(22, 'Reunión con Dany y Forrest', '2019-01-27', '08:00:00', '2019-01-27', '16:00:00', 0, 2),
-(23, 'Asistir a misa con Clarice', '2019-01-27', '19:00:00', '2019-01-27', '20:30:00', 0, 2),
-(24, 'Cita con el kinesiólogo', '2019-01-28', '09:00:00', '2019-01-28', '12:00:00', 0, 2),
-(25, 'Reparar y ajustar motor del carro', '2019-01-29', NULL, NULL, NULL, 1, 2),
-(26, 'Asistir a la gala de premios', '2019-01-30', NULL, NULL, NULL, 1, 2),
-(27, 'Reunión con el asesor financiero y contador', '2019-01-31', '09:00:00', '2019-01-31', '14:00:00', 0, 2),
-(28, 'Fiesta cumpleaños de Clark', '2019-01-31', '17:00:00', '2019-01-31', '23:30:00', 0, 2),
-(29, 'Asistir cita con Alice y Dany', '2019-02-01', NULL, NULL, NULL, 1, 2),
-(30, 'Entrevista con Jimmy y Jay', '2019-02-02', NULL, NULL, NULL, 1, 2);
+(1, 'Ensayo para el casting de pelicula', '2019-02-06', '10:00:00', '2019-02-06', '16:00:00', 0, 4),
+(2, 'Asistir a consulta de odontología ', '2019-02-04', '10:00:00', '2019-02-04', '12:00:00', 0, 4),
+(3, 'Reunión con amigos de club en casa de Robert', '2019-02-03', NULL, NULL, NULL, 1, 4),
+(4, 'Asistir a casting al teatro British Hall', '2019-02-04', '14:00:00', '2019-02-04', '18:00:00', 0, 4),
+(5, 'Cumpleaños de Aldus y Brian', '2019-02-05', NULL, NULL, NULL, 1, 4),
+(6, 'Cita con el doctor Dimitri', '2019-02-07', '09:00:00', '2019-02-07', '10:00:00', 0, 4),
+(7, 'Asistir a la Gala de Premios', '2019-02-10', NULL, NULL, NULL, 1, 4),
+(8, 'Preparación para la Gala de premios', '2019-02-16', '17:00:00', '2019-02-16', '20:00:00', 0, 4),
+(9, 'Reunión con la familia', '2019-02-16', NULL, NULL, NULL, 1, 4),
+(10, 'Prueba y ensayo de escena con Vehículo', '2019-02-08', NULL, NULL, NULL, 1, 4),
+(11, 'Comprar regalo para Dorothy', '2019-02-13', '09:00:00', '2019-02-13', '14:00:00', 0, 3),
+(12, 'Cumpleaños de Dorothy', '2019-02-14', NULL, NULL, NULL, 1, 3),
+(13, 'Reunión con mi hija Alice', '2019-02-03', '17:00:00', '2019-02-03', '22:00:00', 0, 3),
+(14, 'Asistir a la consulta dermatológica', '2019-02-04', '08:00:00', '2019-02-04', '10:00:00', 0, 3),
+(15, 'Cumplir con la clase de Actuación y Doblaje', '2019-02-05', NULL, NULL, NULL, 1, 3),
+(16, 'Compras y pagos mensuales', '2019-02-06', NULL, NULL, NULL, 1, 3),
+(17, 'Comienzo de rodaje de la Película', '2019-02-07', '08:00:00', '2019-02-08', '18:00:00', 0, 3),
+(18, 'Reunión con productores y equipo de riesgo', '2019-02-09', '09:00:00', '2019-02-09', '15:00:00', 0, 3),
+(19, 'Rodaje #2 y 3 de película', '2019-02-10', '13:00:00', '2019-02-10', '23:00:00', 0, 3),
+(20, 'Reunión con Patric sobre escena de drama y riesgo', '2019-02-12', NULL, NULL, NULL, 1, 3),
+(21, 'Asistir al evento Comic-Con de Texas', '2019-02-03', NULL, NULL, NULL, 1, 2),
+(22, 'Reunión con Dany y Forrest', '2019-02-09', '08:00:00', '2019-02-09', '16:00:00', 0, 2),
+(23, 'Asistir a misa con Clarice', '2019-02-09', '19:00:00', '2019-02-09', '20:30:00', 0, 2),
+(24, 'Cita con el kinesiólogo', '2019-02-04', '09:00:00', '2019-02-04', '12:00:00', 0, 2),
+(25, 'Reparar y ajustar motor del carro', '2019-02-05', NULL, NULL, NULL, 1, 2),
+(26, 'Asistir a la gala de premios', '2019-02-06', NULL, NULL, NULL, 1, 2),
+(27, 'Reunión con el asesor financiero y contador', '2019-02-07', '09:00:00', '2019-02-07', '14:00:00', 0, 2),
+(28, 'Fiesta cumpleaños de Clark', '2019-02-07', '17:00:00', '2019-02-07', '23:30:00', 0, 2),
+(29, 'Asistir cita con Alice y Dany', '2019-02-08', NULL, NULL, NULL, 1, 2),
+(30, 'Entrevista con Jimmy y Jay', '2019-02-10', NULL, NULL, NULL, 1, 2);
 
 -- --------------------------------------------------------
 
